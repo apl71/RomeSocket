0aa4c0bb29a853de82b4ef0ac25311f19143f85e
apl71 <luyingjie1998@outlook.com>
2022-10-15 03:15:21 +0000

    更新用于测试的样例，使用新的客户端代码

--- example/echo_client.cpp
+++ example/echo_client.cpp
@@ -5,45 +5,58 @@
 #include <unistd.h>
 #include <string.h>
 #include <mutex>
+#include <fstream>
 #include "thread_pool.hpp"
+#include "client.h"
 
 #define SERVER "localhost"
 #define PORT 8000
-#define BUFFER_SIZE 8192
 
 std::mutex io_mutex;
 
 void go(int id)
 {
-    int sock = socket(PF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in addr;
-    size_t addr_size = sizeof(struct sockaddr_in);
-    memset(&addr, 0, addr_size);
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, SERVER, &addr.sin_addr);
-    connect(sock, (struct sockaddr *)&addr, addr_size);
+    // int sock = socket(PF_INET, SOCK_STREAM, 0);
+    // struct sockaddr_in addr;
+    // size_t addr_size = sizeof(struct sockaddr_in);
+    // memset(&addr, 0, addr_size);
+    // addr.sin_family = AF_INET;
+    // addr.sin_port = htons(PORT);
+    // inet_pton(AF_INET, SERVER, &addr.sin_addr);
+    // connect(sock, (struct sockaddr *)&addr, addr_size);
+    int sock = RomeSocketConnect(SERVER, PORT);
 
-    char send_buff[BUFFER_SIZE];
-    char recv_buff[BUFFER_SIZE];
-    //while (scanf("%s", send_buff))
-    for (int i = 0; i < 100000; ++i)
+    // 读取一个长度大于8192的文件
+    std::ifstream ifs("../../LICENSE", std::ios::in | std::ios::ate | std::ios::binary);
+    if (!ifs.is_open()) {
+        printf("cannot read file\n");
+        return;
+    }
+    auto size = ifs.tellg();
+    ifs.seekg(0, std::ios::beg);
+    char *send_buff = new char[(unsigned)size + 1];
+    ifs.read(send_buff, (unsigned)size);
+    send_buff[size] = 0x00;
+    char recv_buff[8192];
+    for (int i = 0; i < 1; ++i)
     {
-        sprintf(send_buff, "Hello, I'm thread %d, request %d", id, i);
-        send(sock, send_buff, BUFFER_SIZE, 0);
-        recv(sock, recv_buff, BUFFER_SIZE, 0);
+        // sprintf(send_buff, "Hello, I'm thread %d, request %d", id, i);
+        // send(sock, send_buff, BUFFER_SIZE, 0);
+        RomeSocketSend(sock, send_buff, (unsigned)size + 1);
+        recv(sock, recv_buff, 8192, 0);
         io_mutex.lock();
         printf("%s\n", recv_buff);
         io_mutex.unlock();
     }
 
+    ifs.close();
     close(sock);
 }
 
 int main()
 {
-    // go();
-    ThreadPool pool;
-    for (int i = 0; i < 4; ++i)
-        pool.AddTask(go, i);
+    go(1);
+    // ThreadPool pool;
+    // for (int i = 0; i < 4; ++i)
+    //     pool.AddTask(go, i);
 }
