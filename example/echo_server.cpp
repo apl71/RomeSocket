dee7918b69a632a36a671fa3f82d88724a3cd5f8
apl71 <luyingjie1998@outlook.com>
2022-10-17 14:13:44 +0000

    修改调试信息、代码风格，新增异常处理

--- example/echo_server.cpp
+++ example/echo_server.cpp
@@ -1,4 +1,5 @@
 #include "romesocket.hpp"
+#include "exception.hpp"
 #include <iostream>
 
 class EchoServer : public Rocket
@@ -6,21 +7,27 @@ class EchoServer : public Rocket
 public:
     using Rocket::Rocket;
 
-    void OnRead(char *buff, size_t size, int clinet_id) override
-    {
+    void OnRead(char *buff, size_t size, int clinet_id) override {
         std::cout << "I receive a message whose length is: " << size << std::endl;
         char to_write[8192] = "Hello, you say \"";
-        strncat(to_write, buff, 8000);
-        strcat(to_write, "\"");
-        if (Write(to_write, 8192, clinet_id, true) < 0)
-        {
+        for (size_t i = 0; i < size; ++i) {
+            std::cout << buff[i];
+        }
+        std::cout << std::endl;
+        if (Write(to_write, 8192, clinet_id, true) < 0) {
             std::cout << "Error when writing." << std::endl;
         }
     }
 };
 
-int main()
-{
+int main() {
     EchoServer server(8000);
-    server.Start();
+    while (1) {
+        try {
+            server.Start();
+        } catch (SocketException()) {
+            std::cerr << "Fail to create socket. Retry ..." << std::endl;
+            continue;
+        }
+    }
 }
