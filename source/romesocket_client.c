801c1f37c85629de718de4fcbc2c66de45a6f4bc
apl71 <luyingjie1998@outlook.com>
2022-10-27 14:30:36 +0000

    client更名为romesocket_client

--- source/romesocket_client.c
+++ source/romesocket_client.c
@@ -6,6 +6,8 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define HEADER_LENGTH 3
+
 const unsigned BLOCK_SIZE = 8192;
 
 int RomeSocketConnect(const char *server, const unsigned port) {
@@ -21,26 +23,25 @@ int RomeSocketConnect(const char *server, const unsigned port) {
 }
 
 void RomeSocketSend(int sock, const char *buffer, const unsigned size) {
-    unsigned blocks = size / (BLOCK_SIZE - 1) + 1;
+    unsigned blocks = size / (BLOCK_SIZE - HEADER_LENGTH) + 1;
     char *send_buff = malloc(sizeof(char) * BLOCK_SIZE);
     memset(send_buff, 0, BLOCK_SIZE);
-    FILE *file = fopen("../../LICENSE_3", "w");
     for (int i = 0; i < blocks; ++i) {
         memset(send_buff, 0, BLOCK_SIZE);
-        send_buff[0] = (i == blocks - 1) ? 0xFF : (char)i;
-        unsigned start = i * (BLOCK_SIZE - 1), length = BLOCK_SIZE - 1;
+        // 设置消息头
+        unsigned start = i * (BLOCK_SIZE - HEADER_LENGTH), length = BLOCK_SIZE - HEADER_LENGTH;
+        send_buff[0] = i;
         if (i == blocks - 1) {
             length = size - start;
+            send_buff[0] = 0xFF;
         }
-        memcpy(send_buff + 1, buffer + start, length);
-        fwrite(send_buff + 1, sizeof(char), length, file);
-        for (int j = 0; j < length; ++j) {
-            printf("%c", *(send_buff + 1 + j));
-        }
+        send_buff[1] = length >> 8;
+        send_buff[2] = length;
+        memcpy(send_buff + HEADER_LENGTH, buffer + start, length);
         send(sock, send_buff, BLOCK_SIZE, 0);
+        printf("send packet %d: length = %u\n", i, length);
     }
     free(send_buff);
-    fclose(file);
 }
 
 void ReceiveFullBlock(int sock, char *buffer) {
@@ -53,33 +54,36 @@ void ReceiveFullBlock(int sock, char *buffer) {
 }
 
 // 输入一个值为空的指针作为缓冲区
-// 需要手动回收
+// 需要调用RomeSocketClearBuffer回收
 unsigned RomeSocketReceive(int sock, char **buffer) {
     char *temp_list[256] = {NULL};
-    int count = 0;
+    int   length[256] = {0};
+    unsigned count = 0;
+    unsigned total_length = 0;
     while (1) {
         char *temp = malloc(sizeof(char) * BLOCK_SIZE);
         memset(temp, 0, BLOCK_SIZE);
         printf("waiting for block %d\n", count);
         ReceiveFullBlock(sock, temp);
         printf("receive block %d\n", count);
-        temp_list[count++] = temp;
+        temp_list[count] = temp;
+        length[count] = (unsigned)(temp[1] << 8) | ((unsigned)temp[2] & 0x00FF);
+        total_length += length[count++];
         if ((unsigned char)temp[0] == 0xFF) {
             break;
         }
     }
     // 组装
-    size_t total_length = (BLOCK_SIZE - 1) * count;
     *buffer = malloc(sizeof(char) * total_length);
     memset(*buffer, 0, total_length);
     size_t offset = 0;
     for (int i = 0; i < count; ++i) {
-        memcpy(*buffer + offset, temp_list[i] + 1, BLOCK_SIZE - 1);
-        offset += (BLOCK_SIZE - 1);
+        memcpy(*buffer + offset, temp_list[i] + HEADER_LENGTH, length[i]);
+        offset += (BLOCK_SIZE - HEADER_LENGTH);
         // 清除缓存
         free(temp_list[i]);
     }
-    return offset;
+    return total_length;
 }
 
 void RomeSocketClearBuffer(char **buffer) {
