becb7ae7c89ccc3fe7c9b279f8a2eb626d3eeb4d
apl71 <luyingjie1998@outlook.com>
2022-11-23 15:58:08 +0800

    修改Send方法，使用分层逻辑

--- include/client.h
+++ include/client.h
@@ -17,9 +17,9 @@ struct Connection RomeSocketConnect(const char *server, const unsigned port);
 
 void RomeSocketSendAll(int sock, char *send_buff, size_t length);
 
-void RomeSocketSend(struct Connection sock, const char *buffer, const unsigned size);
+void RomeSocketSend(struct Connection sock, struct Buffer send_buffer);
 
-void RomeSocketSplitAndSend(int sock, const char *buffer, const unsigned size);
+// void RomeSocketSplitAndSend(int sock, const char *buffer, const unsigned size);
 
 void RomeSocketReceiveAll(int sock, char *buffer);
 
