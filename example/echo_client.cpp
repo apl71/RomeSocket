fa41f639379cefa44415eeff5438ba6ae177649c
apl71 <luyingjie1998@outlook.com>
2022-11-23 15:57:33 +0800

    使用随机字节流进行测试

--- example/echo_client.cpp
+++ example/echo_client.cpp
@@ -15,35 +15,59 @@
 
 std::mutex io_mutex;
 
+// 长度为0表示随机长度，范围是1-1024*1024字节
+// 需要手动清理返回的数组
+Buffer RandomBytes(unsigned length = 0) {
+    sodium_init();
+    // 随机数
+    while (length == 0) {
+        length = randombytes_uniform(1024 * 100);
+    }
+    char *data = new char[length];
+    randombytes_buf(data, length);
+    return {data, length};
+}
+
+// 比较buffer内容是否相同，相同返回-1，不同则返回第一个不同的位置
+int Compare(Buffer a, Buffer b) {
+    unsigned min_len = a.length < b.length ? a.length : b.length;
+    for (unsigned i = 0; i < a.length; ++i) {
+        if (a.buffer[i] != b.buffer[i]) {
+            return (int)i;
+        }
+    }
+    if (a.length != b.length) {
+        return (int)min_len + 1;
+    }
+    return -1;
+}
+
 void go(int id)
 {
     Connection connection = RomeSocketConnect(SERVER, PORT);
-
-    // 读取一个长度大于8192的文件
-    std::ifstream ifs("../../LICENSE", std::ios::in | std::ios::ate | std::ios::binary);
-    if (!ifs.is_open()) {
-        printf("cannot read file\n");
-        return;
-    }
-    auto size = ifs.tellg();
-    ifs.seekg(0, std::ios::beg);
-    char *send_buff = new char[(unsigned)size];
-    ifs.read(send_buff, (unsigned)size);
-    send_buff[size] = 0x00;
-    for (int i = 0; i < 1; ++i)
+    // 测试数量
+    unsigned test_cases = 20;
+    for (unsigned i = 0; i < test_cases; ++i)
     {
-        // sprintf(send_buff, "Hello, I'm thread %d, request %d", id, i);
-        // send(sock, send_buff, BUFFER_SIZE, 0);
-        RomeSocketSend(connection, send_buff, (unsigned)size);
-        struct Buffer buffer = RomeSocketReceive(connection, 256);
+        // 生成随机数据
+        Buffer data = RandomBytes();
+        RomeSocketSend(connection, data);
+        Buffer buffer = RomeSocketReceive(connection, 256);
+        int result = Compare(data, buffer);
         io_mutex.lock();
         // io
-        std::cout << "get message length: " << buffer.length << std::endl;
+        if (result != -1) {
+            std::cout << std::dec << "Test case " << i << " fail: "
+                      << "Inconstant first occur at " << result << std::endl;
+            std::cout << "length: " << data.length << " and " << buffer.length << std::endl;
+            PrintHex((unsigned char *)data.buffer, data.length);
+            PrintHex((unsigned char *)buffer.buffer, buffer.length);
+        }
         io_mutex.unlock();
-        free(buffer.buffer);
+        delete[]buffer.buffer;
+        delete[]data.buffer;
     }
 
-    ifs.close();
     RomeSocketClose(&connection);
 }
 
