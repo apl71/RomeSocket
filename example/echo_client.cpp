491579a34051be0aea4b27340f305fe7cb066f0a
apl71 <luyingjie1998@outlook.com>
2022-11-23 16:07:55 +0800

    优化测试代码，新增色彩显示

--- example/echo_client.cpp
+++ example/echo_client.cpp
@@ -13,6 +13,10 @@
 #define SERVER "localhost"
 #define PORT 8000
 
+const std::string red   = "\033[31m";
+const std::string green = "\033[32m";
+const std::string reset = "\033[0m";
+
 std::mutex io_mutex;
 
 // 长度为0表示随机长度，范围是1-1024*1024字节
@@ -21,7 +25,7 @@ Buffer RandomBytes(unsigned length = 0) {
     sodium_init();
     // 随机数
     while (length == 0) {
-        length = randombytes_uniform(1024 * 100);
+        length = randombytes_uniform(1024 * 10);
     }
     char *data = new char[length];
     randombytes_buf(data, length);
@@ -46,7 +50,7 @@ void go(int id)
 {
     Connection connection = RomeSocketConnect(SERVER, PORT);
     // 测试数量
-    unsigned test_cases = 20;
+    unsigned test_cases = 2;
     for (unsigned i = 0; i < test_cases; ++i)
     {
         // 生成随机数据
@@ -57,11 +61,14 @@ void go(int id)
         io_mutex.lock();
         // io
         if (result != -1) {
-            std::cout << std::dec << "Test case " << i << " fail: "
+            std::cout << red << "[FAIL]" << reset
+                      << std::dec << "Test case " << i << " fail: "
                       << "Inconstant first occur at " << result << std::endl;
             std::cout << "length: " << data.length << " and " << buffer.length << std::endl;
-            PrintHex((unsigned char *)data.buffer, data.length);
-            PrintHex((unsigned char *)buffer.buffer, buffer.length);
+            // PrintHex((unsigned char *)data.buffer, data.length);
+            // PrintHex((unsigned char *)buffer.buffer, buffer.length);
+        } else {
+            std::cout << green << "[PASS]" << reset << std::dec << "Test case " << i << " pass" << std::endl;
         }
         io_mutex.unlock();
         delete[]buffer.buffer;
