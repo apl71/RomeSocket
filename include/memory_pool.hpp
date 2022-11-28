b811509034e3f2f0735a3275f455079d96dff546
apl71 <luyingjie1998@outlook.com>
2022-11-28 15:34:33 +0800

    新增内存池

--- include/memory_pool.hpp
+++ include/memory_pool.hpp
@@ -3,14 +3,18 @@
 
 #include <vector>
 
-struct SegmentTable {
-    char *start = nullptr;
+// huge block:  BLOCK_SIZE * 1000
+// large block: BLOCK_SIZE * 10
+// small block: BLOCK_SIZE
+
+struct Segment {
+    void *start = nullptr;
     unsigned length = 0;
 };
 
 struct MemoryBlock {
-    char *s = nullptr;
-    std::vector<SegmentTable> segment_table;
+    void *s = nullptr;
+    std::vector<Segment> segment_table;
 };
 
 class MemoryPool {
@@ -19,8 +23,9 @@ private:
     unsigned block_count, block_size;
 
 public:
-    // 构造一个块大小为block_size、共blocks块的内存池
-    MemoryPool(unsigned blocks, unsigned _block_size);
+    // 构造一个内存池，包含的huge block, large block和small block分别为hb, lb和sb
+    // 默认参数会占用约117MB内存
+    MemoryPool(size_t hb = 10, size_t lb = 1000, size_t sb = 10000);
     char *Allocate(unsigned length);
     void Free(char *s);
     ~MemoryPool();
