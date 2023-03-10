912fe0ed3c0a0ce4871c21c57fb20ccf41ea6676
apl71 <luyingjie1998@outlook.com>
2023-03-10 10:54:41 +0800

    多线程性能测试

--- example/benchmark_client.cpp
+++ example/benchmark_client.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <thread>
+#include <mutex>
+
+std::mutex io_mutex;
 
 std::string UnitConversion(double bps) {
     std::vector<std::string> units{"Bps", "KBps", "MBps", "GBps", "TBps"};
@@ -13,37 +18,31 @@ std::string UnitConversion(double bps) {
     return std::to_string(bps) + units[unit];
 }
 
-int main() {
+std::chrono::_V2::system_clock::time_point GetTime() {
+    return std::chrono::system_clock::now();
+}
+
+double GetDuration(std::chrono::_V2::system_clock::time_point start, std::chrono::_V2::system_clock::time_point end) {
+    return double(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den;
+}
+
+void DownloadTest() {
     auto connection = RomeSocketConnect("localhost", 8000, 30);
-    // 先进行下载测试
-    std::cout << "Start uploading test." << std::endl;
-    time_t start = time(nullptr);
+    io_mutex.lock();
+    std::cout << "Start downloading test." << std::endl;
+    io_mutex.unlock();
     long long total_bytes = 0;
-    char buff[8000] = {'u'};
+    auto start = GetTime();
+    char d[] = "d";
+    RomeSocketSend(connection, {d, 2});
     int count = 0;
-    for (int i = 0; i < 100000; ++i) {
-        RomeSocketSend(connection, {buff, 8000});
-        total_bytes += 8000;
-        if (++count % 10000 == 0) {
-            std::cout << count / 10000 << std::endl;
-        }
-    }
-    time_t end = time(nullptr);
-    double speed = (double)total_bytes / (double)(end - start);
-    std::cout << "Average speed: " << UnitConversion(speed) << std::endl;
-    std::cout << "Uploading test ends." << std::endl;
-    // 上传测试
-    std::cout << "Start download test." << std::endl;
-    total_bytes = 0;
-    start = time(nullptr);
-    RomeSocketSend(connection, {"d", 2});
-    count = 0;
     while (true) {
-        //std::cout << count << " ";
         auto response = RomeSocketReceive(connection, 16);
         if (!response.buffer) {
+            io_mutex.lock();
             std::cout << "Test no." << count << " fail." << std::endl;
             std::cout << "Fail to receive message." << std::endl;
+            io_mutex.unlock();
         }
         total_bytes += response.length;
         if (response.buffer[0] == 'e') {
@@ -51,14 +50,60 @@ int main() {
         } else if (response.buffer[0] == 'c') {
             // do nothing
         } else {
+            io_mutex.lock();
             std::cout << "strange." << std::endl;
+            io_mutex.unlock();
         }
         RomeSocketClearBuffer(response);
         ++count;
     }
-    end = time(nullptr);
-    speed = (double)total_bytes / (double)(end - start);
+    auto end = GetTime();
+    auto duration = GetDuration(start, end);
+    double speed = (double)total_bytes / (double)duration;
+    io_mutex.lock();
+    std::cout << "Time: " << duration << " seconds." << std::endl;
     std::cout << "Average speed: " << UnitConversion(speed) << std::endl;
-    std::cout << "Downloading test ends." << std::endl;
+    std::cout << "Downloading test ends.\n" << std::endl;
+    io_mutex.unlock();
     RomeSocketClose(&connection);
+}
+
+void UploadTest(int packs_num) {
+    auto connection = RomeSocketConnect("localhost", 8000, 30);
+    io_mutex.lock();
+    std::cout << "Start uploading test." << std::endl;
+    io_mutex.unlock();
+    auto start = GetTime();
+    long long total_bytes = 0;
+    char buff[8000] = {'u'};
+    for (int i = 0; i < packs_num; ++i) {
+        RomeSocketSend(connection, {buff, 8000});
+        total_bytes += 8000;
+    }
+    auto end = GetTime();
+    double duration = GetDuration(start, end);
+    double speed = (double)total_bytes / (double)duration;
+    io_mutex.lock();
+    std::cout << "Time: " << duration << " seconds." << std::endl;
+    std::cout << "Average speed: " << UnitConversion(speed) << std::endl;
+    std::cout << "Uploading test ends.\n" << std::endl;
+    io_mutex.unlock();
+    RomeSocketClose(&connection);
+}
+
+int main() {
+    std::thread threads[8];
+
+    for (int i = 0; i < 8; ++i) {
+        if (i < 4) {
+            threads[i] = std::thread(UploadTest, 100000);
+        } else {
+            threads[i] = std::thread(DownloadTest);
+        }
+    }
+
+    for (int i = 0; i < 8; ++i) {
+        threads[i].join();
+    }
+    
 }
