0d87d8a85de42ce6dc2e52cdfafbeeda22a49272
apl71 <luyingjie1998@outlook.com>
2023-03-21 15:36:54 +0800

    新增服务端吞吐量统计

--- example/benchmark_server.cpp
+++ example/benchmark_server.cpp
@@ -3,14 +3,48 @@
 #include <iostream>
 #include <unistd.h>
 #include <fstream>
+#include <chrono>
 
 class BenchmarkServer : public Rocket {
+private:
+    static int64_t GetTime() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    }
 public:
     using Rocket::Rocket;
+
+    // 截至上一次统计时，总共传输的字节数
+    int64_t last_bytes = 0;
+    // 当前总共传输的字节数
+    int64_t current_bytes = 0;
+    // 上一次统计的时间
+    int64_t last_time;
+
+    // 每秒执行一次统计
+    void ChronicTask(time_t now) override {
+        // 获取当前时间
+        int64_t current_time = GetTime();
+        // 计算时间差
+        int64_t delta_time = current_time - last_time;
+        // 计算传输字节数的差
+        int64_t delta_bytes = current_bytes - last_bytes;
+        // 计算速度
+        double speed = (double)delta_bytes / (double)delta_time * 1000.0 / 1024.0 / 1024.0;
+        //std::cout << "[" << current_time << "]" << "Speed: " << speed << " MegaBytes per second." << std::endl;
+        std::cout << current_time << " " << speed << std::endl;
+        // 更新统计时间和数据
+        last_time = GetTime();
+        last_bytes = current_bytes;
+    }
+
+    void OnStart() override {
+        last_time = GetTime();
+    }
 
     void OnRead(char *buff, size_t size, int client_id) override {
         if (buff[0] == 'u') {
             // 上传数据测试，不需要做任何事
+            current_bytes += size;
             Pass(client_id);
         } else if (buff[0] == 'd') {
             int data_length = 8000;
@@ -24,9 +58,11 @@ public:
                 if (result <= 0) {
                     std::cout << "Fail to write." << std::endl;
                 }
+                current_bytes += data_length;
             }
             data[0] = 'e';
             result = Write(data, data_length, client_id, true);
+            current_bytes += data_length;
             if (result <= 0) {
                 std::cout << "Fail to write." << std::endl;
             }
